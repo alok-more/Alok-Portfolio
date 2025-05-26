af221ea5f4c16e467afb35f9da9c4197b0e73452
alok-more <alokmore1903@gmail.com>
2025-05-26 23:04:17 +0530

    changes done in files

--- components/shared/sections/experience-section.tsx
+++ components/shared/sections/experience-section.tsx
@@ -1,7 +1,4 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { ExternalLink } from "lucide-react";
 import { experience } from "@/constants";
 
 export function ExperienceSection() {
